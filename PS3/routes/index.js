b9cbd9f1c45b5bb0c464be295cec5c571437e4b5
Taylor Hazlett <thazlett@bu.edu>
2020-02-25 18:49:47 -0500

    PS3 Node App

---
+++ PS3/routes/index.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { string: 'the problem set' });
+});
+
+module.exports = router;
+
